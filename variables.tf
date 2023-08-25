5809fd8b84ac9d1d9639c1f5b33b920e1019846b
Chirag Jain <cjain01@kenvue.com>
2023-08-25 14:34:12 -0400

    Test commit

--- variables.tf
+++ variables.tf
@@ -7,3 +7,9 @@ variable "resource_group_name" {
   default = "sp-tftest-rg"
   description = "Name of the resource group. Updated comment"
 }
+
+
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
